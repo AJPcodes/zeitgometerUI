5483fa5404c1bbde3c268ab50f9813b03760e7e9
AJP <allenatmc@gmail.com>
2016-03-17 17:14:07 -0500

    set up boilerplate with a gulp - browserify eslint and sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,57 @@
-// Include gulp
+// Include Our Plugins
 var gulp = require('gulp');
 
-// Include Our Plugins
+var uglify = require('gulp-uglify');
+var htmlreplace = require('gulp-html-replace');
+var source = require('vinyl-source-stream');
+var browserify = require('browserify');
+var watchify = require('watchify');
+var reactify = require('reactify');
+var streamify = require('gulp-streamify');
 var sass = require('gulp-sass');
 var eslint = require('gulp-eslint');
+var react = require('gulp-react');
 
-// Lint Task
+
+var path = {
+    HTML: './index.html',
+    MINIFIED_OUT: 'build.min.js',
+    OUT: 'build.js',
+    DEST: 'dist',
+    DEST_SRC: 'dist/scripts',
+    DEST_BUILD: 'dist/build',
+    ENTRY_POINT: './scripts/app.js'
+
+};
+
+gulp.task('copy', function(){
+  gulp.src(path.HTML)
+    .pipe(gulp.dest(path.DEST));
+});
+
+gulp.task('watch', function() {
+    gulp.watch('./scripts/**/*.js', ['lint']);
+    gulp.watch('./styles/*.scss', ['sass']);
+  gulp.watch(path.HTML, ['copy']);
+
+  var watcher  = watchify(browserify({
+    entries: [path.ENTRY_POINT],
+    transform: [reactify],
+    debug: true,
+    cache: {}, packageCache: {}, fullPaths: true
+  }));
+
+  return watcher.on('update', function () {
+    watcher.bundle()
+      .pipe(source(path.OUT))
+      .pipe(gulp.dest(path.DEST_SRC))
+      console.log('Updated');
+  })
+    .bundle()
+    .pipe(source(path.OUT))
+    .pipe(gulp.dest(path.DEST_SRC));
+});
+
 
 // Lint Task
 gulp.task('lint', function () {
@@ -60,18 +106,31 @@ gulp.task('lint', function () {
 });
 
 
-// Compile Our Sass
 gulp.task('sass', function() {
     return gulp.src('./styles/*.scss')
         .pipe(sass())
-        .pipe(gulp.dest('./styles/'));
+        .pipe(gulp.dest('dist/styles'));
 });
 
-// Watch Files For Changes
-gulp.task('watch', function() {
-    gulp.watch('./scripts/**/*.js', ['lint']);
-    gulp.watch('./styles/*.scss', ['sass']);
+gulp.task('build', function(){
+  browserify({
+    entries: [path.ENTRY_POINT],
+    transform: [reactify]
+  })
+    .bundle()
+    .pipe(source(path.MINIFIED_OUT))
+    .pipe(streamify(uglify(path.MINIFIED_OUT)))
+    .pipe(gulp.dest(path.DEST_BUILD));
 });
 
-// Default Task
-gulp.task('default', ['lint', 'sass', 'watch'])
+gulp.task('replaceHTML', function(){
+  gulp.src(path.HTML)
+    .pipe(htmlreplace({
+      'js': 'build/' + path.MINIFIED_OUT
+    }))
+    .pipe(gulp.dest(path.DEST));
+});
+
+gulp.task('production', ['replaceHTML', 'build']);
+
+gulp.task('default', ['sass', 'lint', 'watch']);
