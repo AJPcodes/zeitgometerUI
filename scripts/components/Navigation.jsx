9f7a1fe10e4f27480e91599bf2b571dfbf149779
AJP <allenatmc@gmail.com>
2016-03-22 10:10:44 -0500

    make navbar fixed

--- scripts/components/Navigation.jsx
+++ scripts/components/Navigation.jsx
@@ -3,15 +3,17 @@ var Navigation = React.createClass({
   render: function(){
 
     return (
-      <nav>
-        <div className="nav-wrapper">
-          <a href="#" className="brand-logo">Zeitgometer</a>
-          <ul id="nav-mobile" className="right hide-on-med-and-down">
-            <li><a href="#articlesTop">Articles</a></li>
-            <li><a href="#conceptsTop">Concepts</a></li>
-          </ul>
-        </div>
-      </nav>
+      <div className="navbar-fixed">
+        <nav>
+          <div className="nav-wrapper">
+            <a href="#" className="brand-logo">Zeitgometer</a>
+            <ul id="nav-mobile" className="right hide-on-med-and-down">
+              <li><a href="#articlesTop">Articles</a></li>
+              <li><a href="#conceptsTop">Concepts</a></li>
+            </ul>
+          </div>
+        </nav>
+      </div>
     )
   }
 })
